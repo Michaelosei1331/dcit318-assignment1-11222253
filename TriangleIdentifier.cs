57d87ed35315404c0654612bf94789932bbf5356
Michael Osei Bonsu <bmosei002@st.ug.edu.gh>
2025-08-02 22:29:15 +0000

    Implemented Triangle Identifier

---
+++ TriangleIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class TriangleIdentifier
+{
+    public void Run()
+    {
+        Console.Write("Enter the length of side 1: ");
+        int side1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the length of side 2: ");
+        int side2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the length of side 3: ");
+        int side3 = int.Parse(Console.ReadLine());
+
+        if (side1 == side2 && side2 == side3)
+            Console.WriteLine("This is an Equilateral triangle.");
+        else if (side1 == side2 || side1 == side3 || side2 == side3)
+            Console.WriteLine("This is an Isosceles triangle.");
+        else
+            Console.WriteLine("This is a Scalene triangle.");
+    }
+}

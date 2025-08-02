1590009c81b029490eb2ea309dbf3c4a7d138580
Michael Osei Bonsu <bmosei002@st.ug.edu.gh>
2025-08-02 22:27:07 +0000

    Implemented Ticket Price Calculator

---
+++ TicketPriceCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class TicketPriceCalculator
+{
+    public void Run()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age <= 12 || age >= 65)
+            Console.WriteLine("Your ticket price is: GHC7");
+        else
+            Console.WriteLine("Your ticket price is: GHC10");
+    }
+}
